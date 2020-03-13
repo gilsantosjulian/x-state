78a33da62116db54d940dc4e5cabd22b05f1ede0
Julian <julian@minka.io>
2020-03-12 21:50:08 -0500

    feat(index.js) add react-dom render method

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app'
+
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<div>Hola Mundo</div>, rootElement);
