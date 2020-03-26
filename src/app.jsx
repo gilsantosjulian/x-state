718f80a0ceafb841aa7073d9f1efa9f73170e6b3
Julian <julian@minka.io>
2020-03-25 22:58:03 -0500

    feat(app.jsx/redditMachine.js) get subreddits by reddit

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
+import { useMachine } from '@xstate/react';
+import { redditMachine } from './machines/redditMachine';
 
-class App extends React.Component {
-  render() {
-    console.log('entra');
-    
-    return <div>Hola Mundo</div>;
-  }
-}
+const subredits = ['frontend', 'reactjs', 'vuejs'];
+
+const App = () => {
+  const [ state, send ] = useMachine(redditMachine);
+  const { subreddit, posts } = state.context;
+
+  return (
+    <main>
+      <header>
+        <select
+          onChange={e => {
+            send('SELECT', { name: e.target.value });
+          }}
+        >
+          {subredits.map(subreddit => {
+            return <option key={subreddit}>{subreddit}</option>;
+          })}
+        </select>
+      </header>
+
+      <section>
+        <h1>{state.matches('idle') ? 'Select a subreddit' : subreddit}</h1>
+        {state.matches({ selected: 'loading' }) && <div>Loading...</div>}
+        {state.matches({ selected: 'loaded' }) && 
+          <ul>
+          {posts.map(post => (
+            <li key={post.title}>{post.title}</li>
+          ))}
+          </ul>
+        }
+      </section>
+    </main>
+  );
+};
 
 export default App;
