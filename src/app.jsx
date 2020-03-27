9eed8fbd790b3c8f9246e454d29601c04305c9cd
Julian <julian@minka.io>
2020-03-26 20:52:00 -0500

    feat(subReddit.jsx/app.jsx) create subReddit Component

--- src/app.jsx
+++ src/app.jsx
@@ -1,38 +1,26 @@
 import React from 'react';
 import { useMachine } from '@xstate/react';
+import Subreddit from './subReddit.jsx';
 import { redditMachine } from './machines/redditMachine';
 
 const subredits = ['frontend', 'reactjs', 'vuejs'];
 
 const App = () => {
-  const [ state, send ] = useMachine(redditMachine);
-  const { subreddit, posts } = state.context;
+  const [state, send] = useMachine(redditMachine);
+  const { subreddit } = state.context;
 
   return (
     <main>
-      <header>
-        <select
-          onChange={e => {
-            send('SELECT', { name: e.target.value });
-          }}
-        >
-          {subredits.map(subreddit => {
-            return <option key={subreddit}>{subreddit}</option>;
-          })}
-        </select>
-      </header>
-
-      <section>
-        <h1>{state.matches('idle') ? 'Select a subreddit' : subreddit}</h1>
-        {state.matches({ selected: 'loading' }) && <div>Loading...</div>}
-        {state.matches({ selected: 'loaded' }) && 
-          <ul>
-          {posts.map(post => (
-            <li key={post.title}>{post.title}</li>
-          ))}
-          </ul>
-        }
-      </section>
+      <select
+        onChange={e => {
+          send('SELECT', { name: e.target.value });
+        }}
+      >
+        {subredits.map(subreddit => {
+          return <option key={subreddit}>{subreddit}</option>;
+        })}
+      </select>
+      {subreddit && <Subreddit name={subreddit} key={subreddit} />}
     </main>
   );
 };
